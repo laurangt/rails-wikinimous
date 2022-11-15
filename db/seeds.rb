502d6c1ce49395fb865236399f7f117e3ece2484
Laura Nguyen Trong <laurangt@yahoo.com>
2022-11-15 16:58:54 +0100

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  Article.create do |article|
+    article.title = Faker::TvShows::Friends.character
+    article.content = Faker::TvShows::Friends.quote
+  end
+end
